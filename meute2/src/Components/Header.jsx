35f69e8b5ac32bfb51832f510e2919748aa048e1
WhitRavn <jmr4277@orange.fr>
2022-11-19 17:41:58 +0100

    init all files

---
+++ meute2/src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class Header extends React.Component {
+  render() {
+    return (
+      <div className="header-section">
+        <div className="title-box">
+          <h1>Meute2Rats</h1>
+        </div>
+      </div>
+    );
+  }
+}
