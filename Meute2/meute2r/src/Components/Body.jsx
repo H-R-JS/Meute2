5d05ad6f9496bd801acefc75e05ff0f40c21f1c5
Jordy Rocacher <jmr4277@orange.fr>
2023-02-16 20:37:41 +0100

    acceuil page

--- Meute2/meute2r/src/Components/Body.jsx
+++ Meute2/meute2r/src/Components/Body.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 
+const imgAcc = require("../Images/ratAcc.jpeg");
+const imgFoot = require("../Images/pattes.jpeg");
+
 export const Body = () => {
   return (
     <div className="body-page">
       <div className="body-container">
         <div className="img-content">
-          <img className="image"></img>
-          <img className="image-foot"></img>
+          <img src={imgAcc} className="image-acc" />
+          <img src={imgFoot} className="image-foot imf1" />
+          <img src={imgFoot} className="image-foot imf2" />
+          <img src={imgFoot} className="image-foot imf3" />
+          <img src={imgFoot} className="image-foot imf4" />
         </div>
         <div className="main-content">
           <p>
-            Bienvenue dans ma meute de rat, éléveuse de ceux-ci, je vous propose
-            de réserver les rats qui vous plaisent !
+            Bienvenue dans <span>ma meute de rat</span>, éléveuse de ceux-ci, je
+            vous propose de <span>réserver les rats qui vous plaisent !</span>
           </p>
         </div>
       </div>
