b3281fa4af7ab063126f36e9ab6e5e65599117cf
Jordy Rocacher <jmr4277@orange.fr>
2023-02-27 19:51:56 +0100

    little content, css menuPhone

--- Meute2/meute2r/src/Components/ComponentsPhone/MenuPhone.jsx
+++ Meute2/meute2r/src/Components/ComponentsPhone/MenuPhone.jsx
@@ -1,22 +1,15 @@
 import React from "react";
 import { Navbar } from "../HeaderComponents/Navbar";
 
-/*export const MenuPhoneRoute = () => {
-  <Media query="(max-width: 1000px)">
-    {(matches) => {
-      return matches ? <MenuPhone /> : null;
-    }}
-  </Media>;
-};*/
-
 export const MenuPhone = () => {
   return (
     <div>
       <div className="menu-page">
-        <div className="menutitle">
+        <div className="menu-title">
           <h1>Meute2Rats</h1>
-          <div className="header-line"></div>
+          <div className="menu-line"></div>
         </div>
+        <Navbar />
       </div>
     </div>
   );
